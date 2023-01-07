490870a4100981a8ad681001d5d82653d4c69620
DissonantVoid <arandomdude75@gmail.com>
2023-01-07 11:52:30 +0100

    added main scene, camera and some other components

---
+++ game/objects/enemy_bass.gd
@@ -0,0 +1,4 @@
+extends KinematicBody2D
+
+
+var _health : int = 100
---
+++ game/objects/player_star.gd
@@ -0,0 +1,11 @@
+extends "res://game/objects/star_base.gd"
+
+
+
+func _on_body_entered(body):
+	# warn the player or something
+	pass
+
+
+func _on_body_exited(body):
+	pass
